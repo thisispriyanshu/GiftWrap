afe6f03478ed03b81ec251810f6dea45388a2848
thisispriyanshu <priyanshuagrawal801@gmail.com>
2022-07-16 15:29:19 +0530

    client side complete 1st phase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
+import ProductList from "./pages/ProductList";
 import Home from "./pages/Home"
 
 const App=()=>{
-    return <Home/>;
+    return <ProductList/>;
 }
 export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-  padding: 10px 20px;
+  padding: 0px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
